d2ec4d1443bfd0275d10235e78db45f3c4a3ed9a
Aleksey Kiryushin <97094952+lkeey@users.noreply.github.com>
2023-12-29 14:18:10 +0300

    :star: library, onboarding, main, calendar and profile were created

--- app/src/main/java/com/developement/myolimp/feature_onborading/models/FragmentImg.kt
+++
@@ -1,8 +0,0 @@
-package com.developement.myolimp.feature_onborading.models
-
-data class FragmentImg (
-    val img: Int,
-    val isCurrent: Boolean = false,
-    val textTitle: String,
-    val textContent: String
-)
