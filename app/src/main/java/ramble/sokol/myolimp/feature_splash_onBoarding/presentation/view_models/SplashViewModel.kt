3a3c6b1e475156f3941bd4b198dfa757f496905b
Aleksey Kiryushin <97094952+lkeey@users.noreply.github.com>
2024-02-15 22:28:38 +0300

    testing auth

--- app/src/main/java/ramble/sokol/myolimp/feature_splash_onBoarding/presentation/view_models/SplashViewModel.kt
+++ app/src/main/java/ramble/sokol/myolimp/feature_splash_onBoarding/presentation/view_models/SplashViewModel.kt
@@ -11,6 +11,9 @@ import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import ramble.sokol.myolimp.feature_authentication.data.models.City
+import ramble.sokol.myolimp.feature_authentication.data.models.Region
+import ramble.sokol.myolimp.feature_authentication.data.models.School
 import ramble.sokol.myolimp.feature_authentication.domain.repositories.CodeDataStore
 import ramble.sokol.myolimp.feature_profile.database.UserDatabase
 import ramble.sokol.myolimp.feature_profile.domain.repositories.LocalUserRepository
@@ -35,47 +38,90 @@ class SplashViewModel : ViewModel(), KoinComponent {
     val state = _state.asStateFlow()
 
     init {
+//        viewModelScope.launch {
+//            try {
+//                val token = dataStore.getToken(CodeDataStore.COOKIES).first()
+//                    ?: throw Exception("no cookie token")
+//
+//                val response = apiRepository.refreshToken(
+//                    cookie = token,
+//                )
+//
+//                if (response.isSuccessful) {
+//                    Log.i(TAG, "user - ${response.body()?.user}")
+//
+//                    userRepository.deleteUsers()
+//
+//                    Log.i(TAG, "before user - ${userRepository.getUser().firstOrNull()}")
+//
+//                    // save user in local storage
+//                    userRepository.saveUser(response.body()?.user ?: throw Exception("empty body"))
+//
+//                    Log.i(TAG, "after user - ${userRepository.getUser().firstOrNull()}")
+//
+//                    _state.value = LocalUserResult.Success(
+//                        response.body()?.user ?: throw Exception("empty user body")
+//                    )
+//                } else {
+//                    try {
+//                        // delete user from local storage
+//                        userRepository.deleteUsers()
+//
+//                        Log.i(TAG, "user - ${userRepository.getUser().firstOrNull()}")
+//
+//                    } catch (ex: Exception) {
+//                        Log.i(TAG, "exception - $ex")
+//                    }
+//
+//                    _state.value = LocalUserResult.Error(response.message())
+//                }
+//            } catch (ex : Exception) {
+//                Log.i(TAG, "exception - ${ex.message}")
+//                _state.value = LocalUserResult.Error(ex.message)
+//            }
+//        }
+
+//        TODO TESTING
         viewModelScope.launch {
-            try {
-                val token = dataStore.getToken(CodeDataStore.COOKIES).first()
-                    ?: throw Exception("no cookie token")
-
-                val response = apiRepository.refreshToken(
-                    cookie = token,
+            userRepository.saveUser(
+                LocalUserModel(
+                    id = "1010101",
+                    firstName = "Aleksey",
+                    secondName = "Kiryushin",
+                    thirdName = "Aleksandrovich",
+                    dateOfBirth = "14.06.2007",
+                    gender = "m",
+                    snils = "19750386",
+                    phone = "79999999",
+                    email = "kir@mail.ru",
+                    grade = 10,
+                    accountType = "s",
+                    subjects = listOf("Литература", "Информатика"),
+                    region = Region(12, "Москвоская область"),
+                    city = City(1, "Чехов", 12),
+                    school = School(1, "МБОУ СОШ № 35", 12),
                 )
-
-                if (response.isSuccessful) {
-                    Log.i(TAG, "user - ${response.body()?.user}")
-
-                    userRepository.deleteUsers()
-
-                    Log.i(TAG, "before user - ${userRepository.getUser().firstOrNull()}")
-
-                    // save user in local storage
-                    userRepository.saveUser(response.body()?.user ?: throw Exception("empty body"))
-
-                    Log.i(TAG, "after user - ${userRepository.getUser().firstOrNull()}")
-
-                    _state.value = LocalUserResult.Success(
-                        response.body()?.user ?: throw Exception("empty user body")
-                    )
-                } else {
-                    try {
-                        // delete user from local storage
-                        userRepository.deleteUsers()
-
-                        Log.i(TAG, "user - ${userRepository.getUser().firstOrNull()}")
-
-                    } catch (ex: Exception) {
-                        Log.i(TAG, "exception - $ex")
-                    }
-
-                    _state.value = LocalUserResult.Error(response.message())
-                }
-            } catch (ex : Exception) {
-                Log.i(TAG, "exception - ${ex.message}")
-                _state.value = LocalUserResult.Error(ex.message)
-            }
+            )
+            _state.value = LocalUserResult.Success(
+                LocalUserModel(
+                    id = "1010101",
+                    firstName = "Aleksey",
+                    secondName = "Kiryushin",
+                    thirdName = "Aleksandrovich",
+                    dateOfBirth = "14.06.2007",
+                    gender = "m",
+                    snils = "19750386",
+                    phone = "79999999",
+                    email = "kir@mail.ru",
+                    grade = 10,
+                    accountType = "s",
+                    subjects = listOf("Литература", "Информатика"),
+                    region = Region(12, "Москвоская область"),
+                    city = City(1, "Чехов", 12),
+                    school = School(1, "МБОУ СОШ № 35", 12),
+                )
+            )
         }
+//        TODO END TESTING
     }
 }
